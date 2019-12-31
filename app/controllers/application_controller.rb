753b29a87940683f278278f6233bf0b573055f8c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-31 23:06:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(params)
-    redirect '/recipies'
+    redirect '/recipies/#{}'
   end
 
   delete '/recipes/:id/delete' do
